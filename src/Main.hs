1ad9de5a52f532e5fa853b3c578ff6d7d429206e
Ilya Portnov <portnov@bk.ru>
2019-09-15 18:03:19 +0500

    Use trie for rules.

--- src/Main.hs
+++ src/Main.hs
@@ -12,17 +12,45 @@ import System.Environment
 
 data Problem = Problem {
     problemInitState :: IS.IntSet
-  , problemRules :: [Rule]
+  , problemRules :: RulesTrie
   }
   deriving (Show)
 
 data Rule = Rule {
-    ruleFrom :: IS.IntSet
+    ruleFrom :: [Bool]
   , ruleTo :: Bool
   }
   deriving (Show)
 
 type Solver a = St.StateT IS.IntSet IO a
+
+data RulesTrie =
+    EmptyTrie
+  | Leaf Bool
+  | Branch {
+      branchFalse :: RulesTrie,
+      branchTrue :: RulesTrie
+    }
+  deriving (Show)
+
+insertTrie :: [Bool] -> Bool -> RulesTrie -> RulesTrie
+insertTrie [] r EmptyTrie = Leaf r
+insertTrie (x : xs) r EmptyTrie
+  | x = Branch EmptyTrie (insertTrie xs r EmptyTrie)
+  | otherwise = Branch (insertTrie xs r EmptyTrie) EmptyTrie
+insertTrie (x : xs) r (Branch f t)
+  | x = Branch f (insertTrie xs r t)
+  | otherwise = Branch (insertTrie xs r f) t
+insertTrie path r trie =
+  error $ "unexpected: " ++ show path ++ ", " ++ show r ++ ", " ++ show trie
+
+mkTrie :: [Rule] -> RulesTrie
+mkTrie rules = foldr insertRule EmptyTrie rules
+  where
+    insertRule r trie =
+      insertTrie (ruleFrom r) (ruleTo r) trie
+
+    ruleList set = [i `IS.member` set | i <- [-2 .. 2]]
 
 mkSet :: [Int] -> [Bool] -> IS.IntSet
 mkSet idxs mask =
@@ -36,10 +64,9 @@ pInitState = do
 
 pRule :: Parser Rule
 pRule = do
-  fromSt <- replicateM 5 pPot
+  from <- replicateM 5 pPot
   string " => "
   to <- pPot
-  let from = mkSet [-2 .. 2] fromSt
   return $ Rule from to
 
 pPot :: Parser Bool
@@ -52,37 +79,27 @@ pProblem = do
   initSt <- pInitState
   newline
   rules <- pRule `sepEndBy` newline
-  return $ Problem initSt rules
+  return $ Problem initSt $ mkTrie rules
 
-checkRule :: Int -> Rule -> Solver (Maybe Bool)
-checkRule i (Rule {..}) = do
-    matches <- check (-2)
-    if matches
-      then return $ Just ruleTo
-      else return Nothing
+checkRules :: Int -> RulesTrie -> Solver (Maybe Bool)
+checkRules i trie = do
+    check (-2) trie
   where
-    check :: Int -> Solver Bool
-    check 3 = return True
-    check j = do
+    check :: Int -> RulesTrie -> Solver (Maybe Bool)
+    check 3 (Leaf r) = return $ Just r
+    check _ EmptyTrie = return Nothing
+    check j rules = do
       oldGeneration <- get
-      let rulePot = j `IS.member` ruleFrom
-          actualPot = (i+j) `IS.member` oldGeneration
-      if rulePot == actualPot
-        then check (j+1)
-        else return False
-
-checkRules :: Int -> [Rule] -> Solver (Maybe Bool)
-checkRules _ [] = return Nothing
-checkRules i (rule : rules) = do
-  r <- checkRule i rule
-  case r of
-    Nothing -> checkRules i rules
-    Just new -> return (Just new)
+      let actualPot = (i+j) `IS.member` oldGeneration
+      let rules' = if actualPot
+                     then branchTrue rules
+                     else branchFalse rules
+      check (j+1) rules'
 
 runGeneration :: Integer -> Problem -> Solver Bool
 runGeneration generationNumber (Problem {..}) = do
     oldGeneration <- get
-    when (generationNumber `mod` 1000 == 0) $
+    when (generationNumber `mod` 10000 == 0) $
         lift $ putStrLn $ show generationNumber ++ ": " ++ showGeneration oldGeneration
 
     newPairs <-
@@ -97,15 +114,17 @@ runGeneration generationNumber (Problem {..}) = do
            put newGeneration
            return False
 
-solve :: Problem -> Solver ()
-solve p@(Problem {..}) = go 1
+solve :: Integer -> Problem -> Solver ()
+solve generations p@(Problem {..}) = go 1
   where
-    go 50000000000 = return ()
     go n = do
-      stop <- runGeneration n p
-      if stop
+      if n == generations+1
         then return ()
-        else go (n+1)
+        else do
+          stop <- runGeneration n p
+          if stop
+            then return ()
+            else go (n+1)
 
 showGeneration :: IS.IntSet -> String
 showGeneration gen = 
@@ -120,12 +139,12 @@ showGeneration gen =
 
 main :: IO ()
 main = do
-  [path] <- getArgs
+  [generations, path] <- getArgs
   r <- parseFromFile pProblem path
   case r of
     Left err -> fail (show err)
     Right problem -> do
-      generation <- execStateT (solve problem) (problemInitState problem)
+      generation <- execStateT (solve (read generations) problem) (problemInitState problem)
       putStrLn $ showGeneration generation
       let answer = sum $ IS.toList generation
       print answer
